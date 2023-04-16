cabda1f40da1d3d99e0629b8375d39f150cf415a
XiaoYoung <xy_23@outlook.com>
2023-04-16 10:16:22 +0800

    添加时间戳和硬触发的测试

--- camera_test.py
+++ camera_test.py
@@ -2,10 +2,15 @@ from modules.io.mindvision import Camera
 from remote_visualizer import Visualizer
 
 if __name__ == '__main__':
-    with Camera(4) as camera, Visualizer() as visualizer:
+    with Camera(4, True) as camera, Visualizer() as visualizer:
+        last_stamp_ms = 0
         while True:
             success, img = camera.read()
             if not success:
                 continue
+
+            stamp_ms = camera.get_stamp_ms()
+            print(f'{stamp_ms - last_stamp_ms}ms')
+            last_stamp_ms = stamp_ms
 
             visualizer.show(img)
