e484cf1e7d3ad0049eb79381a961ddb63968acc2
XiaoYoung <xy_23@outlook.com>
2023-04-18 23:18:06 +0800

    枪管偏移量

--- main_without_predict.py
+++ main_without_predict.py
@@ -12,13 +12,13 @@ from modules.armor_detection import ArmorDetector
 from remote_visualizer import Visualizer
 
 if __name__ == '__main__':
-    with Robot(3, '/dev/ttyUSB0') as robot, Visualizer(enable=False) as visualizer:
+    with Robot(4, '/dev/ttyUSB0') as robot, Visualizer(enable=False) as visualizer:
         
         robot.update()
         if robot.id == 3:
-            from configs.infantry3 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal
+            from configs.infantry3 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
         elif robot.id == 4:
-            from configs.infantry4 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal
+            from configs.infantry4 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
 
         armor_detector = ArmorDetector(cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal)
 
@@ -30,7 +30,23 @@ if __name__ == '__main__':
             armors = list(filter(lambda a: a.color != robot.color, armors))
             armors.sort(key=lambda a: a.observation[0]) # 优先击打最近的
 
+            if len(armors) > 0:
+                armor = armors[0]
+                
+                cx, cy, cz = armor.in_camera.T[0]
+                x, y, z = armor.in_imu.T[0]
+                imu_yaw = armor.yaw_in_imu
+
+                pitch = tools.shoot_pitch(x, y, z, robot.bullet_speed) + pitch_offset
+                armor_in_gun = armor.in_imu.copy()
+                armor_in_gun[1] = (x*x + z*z) ** 0.5 * -math.tan(math.radians(pitch))
+
+                robot.send(*armor_in_gun.T[0])
+
             # 调试用
+            if not visualizer.enable:
+                continue
+
             drawing = img.copy()
             for i, l in enumerate(armor_detector._filtered_lightbars):
                 tools.drawContour(drawing, l.points, (0, 255, 255), 10)
@@ -41,21 +57,7 @@ if __name__ == '__main__':
                 tools.drawAxis(drawing, a.center, a.rvec, a.tvec, cameraMatrix, distCoeffs)
                 tools.putText(drawing, f'{i} {a.color} {a.name} {a.confidence:.2f}', a.left.top, (255, 255, 255))
                 tools.putText(drawing, f'cx{cx:.1f} cy{cy:.1f} cz{cz:.1f}', a.left.bottom, (255, 255, 255))
+            
             visualizer.show(drawing)
 
-            if len(armors) > 0:
-                armor = armors[0]
-                
-                cx, cy, cz = armor.in_camera.T[0]
-                x, y, z = armor.in_imu.T[0]
-                imu_yaw = armor.yaw_in_imu
-
-                pitch = tools.shoot_pitch(x, y, z, robot.bullet_speed) + 4
-                armor_in_gun = armor.in_imu.copy()
-                armor_in_gun[1] = (x*x + z*z) ** 0.5 * -math.tan(math.radians(pitch))
-                print(f'{pitch} {armor.in_imu[1,0]} {armor_in_gun[1,0]}')
-
-                robot.send(*armor_in_gun.T[0])
-
-                # 调试用
-                visualizer.plot((imu_yaw, robot.yaw, robot.pitch), ('imu_yaw', 'yaw', 'pitch'))
+            visualizer.plot((imu_yaw, robot.yaw, robot.pitch), ('imu_yaw', 'yaw', 'pitch'))
