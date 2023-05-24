f10690810d0eb3a9cdd6e51dee4283c3cb6b9a91
XiaoYoung <xy_23@outlook.com>
2023-05-24 18:33:27 +0800

    bugfix

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
             if len(armors) > 0:
                 armor = armors[0]
-                visualizer.plot((armor.in_camera_mm[0, 0]/10, armor.yaw_in_camera_degree,), ('x', 'yaw', ))
+                # visualizer.plot((armor.in_camera_mm[0, 0]/10, armor.yaw_in_camera_degree,), ('x', 'yaw', ))
 
             # visualizer.show(armor_detector._gray_img)
             visualizer.show(drawing)
