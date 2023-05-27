9c96c4629b60988771f357d5f8dad52ae073c723
XiaoYoung <xy_23@outlook.com>
2023-05-27 18:40:18 +0800

    bugfix

--- static_shoot.py
+++ static_shoot.py
@@ -36,13 +36,13 @@ if __name__ == '__main__':
         robot.update()
 
         if robot.id == 1:
-            from configs.hero import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
+            from configs.hero import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, gun_up_degree, gun_right_degree
         elif robot.id == 3:
-            from configs.infantry3 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
+            from configs.infantry3 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, gun_up_degree, gun_right_degree
         elif robot.id == 4:
-            from configs.infantry4 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
+            from configs.infantry4 import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, gun_up_degree, gun_right_degree
         elif robot.id == 7:
-            from configs.sentry import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, pitch_offset
+            from configs.sentry import cameraMatrix, distCoeffs, R_camera2gimbal, t_camera2gimbal, gun_up_degree, gun_right_degree
 
         enemy_color = 'red' if robot.color == 'blue' else 'blue'
         armor_detector = ArmorDetector(enemy_color)
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 
                 aim_point_m[1, 0] = shoot_y_mm / 1e3
 
-                robot.shoot(pitch_offset, aim_point_m)
+                robot.shoot(gun_up_degree, gun_right_degree, aim_point_m)
 
             # 调试分割线
 
